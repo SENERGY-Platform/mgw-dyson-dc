a84f66f486839ae7f19f1484dadd2365b99c641f
y-du <42994541+y-du@users.noreply.github.com>
2021-01-19 11:51:28 +0100

    add all services and generators

--- dyson/model.py
+++ dyson/model.py
@@ -19,30 +19,57 @@ __all__ = ("Model", "model_map")
 
 
 from util import conf
+from . import service
 import typing
 
 
 class Model:
-    def __init__(self, id: str, type: str, services: dict, push_dev_ste_srv: typing.Optional[typing.Callable[[dict], typing.Any]] = None, push_sen_rdg_srv: typing.Optional[typing.Callable[[dict], typing.Any]] = None):
+    def __init__(
+            self,
+            id: str,
+            type: str,
+            msg_type_field: str,
+            device_state_msg_types: tuple,
+            services: dict,
+            gen_state_req_msg: typing.Callable,
+            gen_sensor_data_req_msg: typing.Optional[typing.Callable] = None,
+            sensor_data_msg_types: typing.Optional[tuple] = tuple(),
+            push_state_srv: typing.Optional[typing.Tuple[str, typing.Callable]] = None,
+            push_readings_srv: typing.Optional[typing.Tuple[str, typing.Callable]] = None
+    ):
         self.id = id
         self.type = type
+        self.msg_type_field = msg_type_field
         self.services = services
-        self.push_dev_ste_srv = push_dev_ste_srv
-        self.push_sen_rdg_srv = push_sen_rdg_srv
+        self.gen_state_req_msg = gen_state_req_msg
+        self.gen_sensor_data_req_msg = gen_sensor_data_req_msg
+        self.device_state_msg_types = device_state_msg_types
+        self.sensor_data_msg_types = sensor_data_msg_types
+        self.push_state_srv = push_state_srv
+        self.push_readings_srv = push_readings_srv
 
 
 pure_cool_link = Model(
     id="475",
     type=conf.Senergy.dt_pure_cool_link,
+    msg_type_field="msg",
     services={
-        "setPower": None,
-        "setOscillation": None,
-        "setSpeed": None,
-        "setMonitoring": None,
-        "getDeviceState": None
+        "setPower": service.pcl_475.set_power,
+        "getPower": service.pcl_475.get_power,
+        "setOscillation": service.pcl_475.set_oscillation,
+        "getOscillation": service.pcl_475.get_oscillation,
+        "setSpeed": service.pcl_475.set_speed,
+        "getSpeed": service.pcl_475.get_speed,
+        "setMonitoring": service.pcl_475.set_monitoring,
+        "getMonitoring": service.pcl_475.get_monitoring,
+        "getFilterLife": service.pcl_475.get_filter_life
     },
-    push_dev_ste_srv=None,
-    push_sen_rdg_srv=None
+    gen_state_req_msg=service.pcl_475.gen_device_state_req_msg,
+    gen_sensor_data_req_msg=service.pcl_475.gen_sensor_data_req_msg,
+    device_state_msg_types=("CURRENT-STATE", "STATE-CHANGE"),
+    sensor_data_msg_types=("ENVIRONMENTAL-CURRENT-SENSOR-DATA", ),
+    push_state_srv=("getDeviceState", service.pcl_475.push_device_state),
+    push_readings_srv=("getSensorReadings", service.pcl_475.push_sensor_readings)
 )
 
 model_map = {
