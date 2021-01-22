20bd1e923a81e7902fada54fe3ef9bed82a9d52e
y-du <42994541+y-du@users.noreply.github.com>
2021-01-22 13:10:44 +0100

    split services

--- dyson/model.py
+++ dyson/model.py
@@ -29,7 +29,8 @@ class Model:
             type: str,
             msg_type_field: str,
             device_state_msg_types: tuple,
-            services: dict,
+            set_services: dict,
+            get_services: dict,
             gen_state_req_msg: typing.Callable,
             gen_sensor_data_req_msg: typing.Optional[typing.Callable] = None,
             sensor_data_msg_types: typing.Optional[tuple] = tuple(),
@@ -38,7 +39,8 @@ class Model:
     ):
         self.type = type
         self.msg_type_field = msg_type_field
-        self.services = services
+        self.set_services = set_services
+        self.get_services = get_services
         self.gen_state_req_msg = gen_state_req_msg
         self.gen_sensor_data_req_msg = gen_sensor_data_req_msg
         self.device_state_msg_types = device_state_msg_types
@@ -50,14 +52,16 @@ class Model:
 pure_cool_link = Model(
     type=conf.Senergy.dt_pure_cool_link,
     msg_type_field="msg",
-    services={
+    set_services={
         "setPower": service.pcl_475.set_power,
-        "getPower": service.pcl_475.get_power,
         "setOscillation": service.pcl_475.set_oscillation,
-        "getOscillation": service.pcl_475.get_oscillation,
         "setSpeed": service.pcl_475.set_speed,
-        "getSpeed": service.pcl_475.get_speed,
         "setMonitoring": service.pcl_475.set_monitoring,
+    },
+    get_services={
+        "getPower": service.pcl_475.get_power,
+        "getOscillation": service.pcl_475.get_oscillation,
+        "getSpeed": service.pcl_475.get_speed,
         "getMonitoring": service.pcl_475.get_monitoring,
         "getFilterLife": service.pcl_475.get_filter_life
     },
